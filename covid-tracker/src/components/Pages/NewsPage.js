538236e6c18fddb71629d75bd4584582b05ed297
PratilipiAich <pratilipiaich@gmail.com>
2020-06-19 15:05:06 +0530

    Added NewsPage and modified Home path

---
+++ covid-tracker/src/components/Pages/NewsPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+function NewsPage() {
+    return(
+      <div  >
+        <center>
+        <h1>News</h1>
+        <p>This is News page.</p>
+
+        </center>
+      
+        
+      </div>
+    )
+  }
+  export default  NewsPage;
